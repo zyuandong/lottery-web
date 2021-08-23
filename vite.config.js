7706f650123cea1a668aa0ac281dde7b88247cfa
zyuandong <de1719@163.com>
2021-08-23 17:44:17 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [vue()],
+  server: {
+    hmr: true,
+    port: 3001,
+    open: false,
+    proxy: {
+      "/lottery_service_api": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/lettory_service_api/, '')
+      }
+    },
+  },
+});
