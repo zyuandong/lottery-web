a26f755e4498c52291e876bfeaff606b3d6249c7
zyuandong <de1719@163.com>
2021-08-23 17:49:47 +0800

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,10 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
+import { resolve } from 'path';
+
+const pathResolve = (dir) => {
+  return resolve(process.cwd(), '.', dir)
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -16,4 +21,9 @@ export default defineConfig({
       }
     },
   },
+  resolve: {
+    alias: {
+      '@': pathResolve('./src')
+    }
+  }
 });
