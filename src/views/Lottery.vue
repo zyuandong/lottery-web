963e63f1618846ae7b79fb94bdcd973480588722
zyuandong <de1719@163.com>
2021-08-24 18:31:51 +0800

    see feat: [animation]

--- src/views/Lottery.vue
+++ src/views/Lottery.vue
@@ -6,19 +6,19 @@
           <div class="lottery-border">
             <el-row class="p-8">
               <el-col :span="8">
-                <div class="prize-item is-active">
+                <div class="prize-item item-1 is-active">
                   <img src="../assets/260.jpg" alt="" />
                   <div class="text">Switch</div>
                 </div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">
+                <div class="prize-item item-2">
                   <img src="../assets/260.jpg" alt="" />
                   <div class="text">Vue</div>
                 </div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">
+                <div class="prize-item item-3">
                   <img src="../assets/260.jpg" alt="" />
                   <!-- <img
                     src="https://i.loli.net/2021/06/03/YNIqRlXbKo6ZEhp.png"
@@ -28,25 +28,25 @@
                 </div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">4</div>
+                <div class="prize-item item-4">4</div>
               </el-col>
               <el-col :span="8">
-                <div class="btn-lottery">
+                <div class="btn-lottery" @click="runAnimation()">
                   <div class="text-lottery">抽奖</div>
                   <div class="text">100 金币 / 次</div>
                 </div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">6</div>
+                <div class="prize-item item-6">6</div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">7</div>
+                <div class="prize-item item-7">7</div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">8</div>
+                <div class="prize-item item-8">8</div>
               </el-col>
               <el-col :span="8">
-                <div class="prize-item">9</div>
+                <div class="prize-item item-9">9</div>
               </el-col>
             </el-row>
           </div>
@@ -60,7 +60,56 @@
 </template>
 
 <script>
-export default {};
+import { ref } from 'vue';
+export default {
+  setup() {
+    const arr = [1, 2, 3, 6, 9, 8, 7, 4];
+    let index = 0;
+    let timer = null;
+
+    const sendRequest = () => {
+      let closeTimer = null;
+      setTimeout(() => {
+        closeTimer = setInterval(() => {
+          if (index = 5) {
+            clearInterval(timer);
+            timer = null;
+          }
+        }, 100)
+        if (!timer) {
+          clearInterval(closeTimer);
+        }
+      }, 5000)
+    };
+
+    const runAnimation = () => {
+      console.log(index);
+      if (!timer) {
+        // sendRequest();
+        timer = setInterval(() => {
+          document
+            .querySelector(`.item-${arr[index]}`)
+            .classList.remove('is-active');
+
+          document
+            .querySelector(`.item-${arr[index === 7 ? 0 : index + 1]}`)
+            .classList.add('is-active');
+          index = index === 7 ? 0 : index + 1;
+          // console.log(index);
+        }, 100);
+      } else {
+        clearInterval(timer);
+        timer = null;
+      }
+    };
+
+    // setInterval()
+
+    return {
+      runAnimation,
+    };
+  },
+};
 </script>
 
 <style lang="scss" scoped>
