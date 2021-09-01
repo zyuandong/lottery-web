bc2fc4cde82d4d01e071ac335c8cc6abfc6622a7
zyuandong <de1719@163.com>
2021-09-01 14:33:22 +0800

    [404] style: update 404 page style

--- src/views/404.vue
+++ src/views/404.vue
@@ -1,17 +1,21 @@
 <template>
   <div id="page-404">
-    404 Error
-    Back to 
-    <router-link to="/lottery">lottery</router-link>
+    <div class="text-404">404 - Page Not Found</div>
+    <div class="m-t-24">Back to <router-link to="/lottery">Lottery</router-link></div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
+<style lang="scss">
+#page-404 {
+  text-align: center;
+  margin-top: calc(100vh / 5);
 
+  .text-404 {
+    font-size: 0.4rem;
+  }
+}
 </style>
